a9010564c130181a1a373a963d358c37367cea8b
agnieszkamuszynska <agnieszka.muszynska@openx.com>
2025-03-21 14:12:36 +0100

    config files

---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+# React + TypeScript + Vite
+
+## Running the UI
+
+1. Build the app: ```npm run build```
+2. Run the app: ```npm run dev```
---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "./components/LoginForm";
+
+function App() {
+    return (
+        <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+            <h1>Welcome! – Please Log In</h1>
+            <LoginForm />
+        </div>
+    );
+}
+
+export default App;
